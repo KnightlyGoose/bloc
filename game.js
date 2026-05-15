7f3e9526fb62dfab5a13165b5b6d6c6fdd80170e
Night-Owl-Knight <117783808+Night-Owl-Knight@users.noreply.github.com>
2026-05-15 10:36:22 -0500

    Refactor state structure into categories adn replace invest and sell placeholder functions with generic changeStat system

--- game.js
+++ game.js
@@ -2,6 +2,7 @@
 const state = {
 
   resources: {
+    clamped: false,
     money: 10,
     oil: 5,
     material: 5,
@@ -9,69 +10,87 @@ const state = {
   },
 
   domestic: {
+    clamped: true,
     stability: 10,
     popularity: 10,
     oppression: 10,
-    capitalism: 50
+    capitalism: 50,
+    military_loyalty: 50,
+
 
   },
 
   military: {
+    clamped: false,
     strength: 10,
     size: 10,
     airforce: 0,
     weapons: 50,
     tech_level: 1,
-    loyalty: 50
   },
 
   foreign: {
+    clamped: true,
     eagle_relations: 0,
-    bear_relations: 0
+    bear_relations: 0,
+    triangle_relations: 50
+
   }
    
     
    
 }
 
+const clamp = (val ,min, max) => Math.min(Math.max(val, min), max);
 
 function changeStat(category, stat, amount) {
 
-  if (!state[category] || state[category][stat] == undefined) {
+  const group = state[category];
+  
+
+  if (!group || group[stat] == undefined) {
     console.error("Invalid stat: ", category, stat);
     return;
   }
+
   state[category][stat] += amount;
+
+  if (group.clamped) {
+    group[stat] = clamp(group[stat], 0, 100);
+  }
 }
 
 
  function update() {
-  document.getElementById("money").textContent = state.money;
-  document.getElementById("stability").textContent = state.stability;
-  document.getElementById("oil").textContent = state.oil;
-  document.getElementById("material").textContent = state.material;
-  document.getElementById("goods").textContent = state.goods;
+  document.getElementById("money").textContent = state.resources.money;
+  document.getElementById("stability").textContent = state.domestic.stability;
+  document.getElementById("oil").textContent = state.resources.oil;
+  document.getElementById("material").textContent = state.resources.material;
+  document.getElementById("goods").textContent = state.resources.goods;
 
-  if (state.stability <= 0) {
+  checkLoss();
+  
+ 
+}
+
+function invest() {
+  changeStat("resources", "money", 100);
+}
+
+function checkLoss() {
+  if (state.foreign.triangle_relations <= 0)  {
+    alert("Triangle");
+  }
+
+   if (state.stability <= 0) {
     alert("Coup! Game Over.");
+
   }
 
   if (state.money <= 0) {
     alert("Bankrupt! Game Over.");
   }
-}
 
-function invest() {
-  state.money -= 2;
-  state.material += 2;
-  state.stability -= 1;
-  update();
-}
-
-function sell() {
-  state.goods -= 1;
-  state.money += 3;
-  update();
 }
 
 update();
